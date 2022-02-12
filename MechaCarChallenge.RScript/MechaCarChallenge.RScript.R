7a1d9dc024d452944771b81c5b9accd980a88e25
Ramya Ramamurthy <ramya.rr@gmail.com>
2022-02-11 17:26:08 -0800

    Adding ## Summary Statistics on Suspension Coils to MechaCarChallenge.RScript

--- MechaCarChallenge.RScript/MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript/MechaCarChallenge.RScript.R
@@ -11,3 +11,22 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Perform summary()
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= mechcar_df))
+
+# Deliverable 2 : Visualizations for the Trip Analysis
+
+#Import and read in the Suspension_Coil.csv file as a table.
+suspension_df <- read.csv(file = "../Suspension_Coil.csv", check.names=F, stringsAsFactors = F)
+
+#creates a total_summary dataframe using the summarize() function to get 
+#the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- suspension_df %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI), .groups = 'keep') 
+
+#creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by 
+#the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+lot_summary <- suspension_df  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Variance=var(PSI),
+                                                                         SD=sd(PSI), .groups = 'keep')      
